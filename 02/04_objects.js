9a64dceb8af97cef17bf7d6d60bd5ec2af8a0f76
Vivek Singh <133307528+vivekcrox@users.noreply.github.com>
2024-11-14 11:31:01 +0000

    ongoing

--- 02/04_objects.js
+++ 02/04_objects.js
@@ -24,10 +24,14 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// console.log(obj3);   //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+
+const obj3 = Object.assign({}, obj1, obj2, obj4)
+console.log(obj3);  //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+// const obj3 = {...obj1, ...obj2}    //spread operator
+console.log(obj3);  //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 
 const users = [
@@ -48,11 +52,11 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));   //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser));  //[ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser));   //[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  //true
 
 
 const course = {
@@ -63,7 +67,8 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+//destructing
+const {courseInstructor: instructor} = course    //now can call using 'instructor'
 
 // console.log(courseInstructor);
 console.log(instructor);
@@ -79,3 +84,10 @@ console.log(instructor);
     {},
     {}
 ]
+
+
+//React taste 
+const navbar = ({company}) => {
+
+}
+navbar(company = "vivek")
